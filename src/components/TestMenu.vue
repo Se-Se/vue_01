d78e8a6b93968340168a9034029fb3b455226ffe
chen <chenbo9191110@163.com>
2025-05-17 03:43:50 +0800

    feat: testMenu

--- src/components/TestMenu.vue
+++ src/components/TestMenu.vue
@@ -1,68 +1,66 @@
 <template>
-  <a-layout-sider
-    v-model:collapsed="collapsed"
-    :collapsedWidth="0"
-    width="200"
-    style="background: #fff"
-    :trigger="null"
-  >
-    <a-menu
-      mode="inline"
-      :selectedKeys="selectedKeys"
-      :openKeys="openKeys"
-      @select="onSelect"
-      @openChange="onOpenChange"
-      style="height: 100%"
-    >
-      <!-- 渲染顶级菜单项 -->
-      <a-menu-item
-        v-for="item in topLevelItems"
-        :key="item.key"
-        @click="handleMenuClick(item)"
-      >
-        {{ item.label }}
-      </a-menu-item>
-
-      <!-- 渲染带子菜单的项 -->
-      <a-sub-menu
-        v-for="item in subMenuItems"
-        :key="item.key"
-      >
-        <template #title>{{ item.label }}</template>
-        <a-menu-item
-          v-for="child in item.children"
-          :key="child.key"
-          @click="handleMenuClick(child)"
-        >
-          {{ child.label }}
+  <a-layout-sider v-model:collapsed="collapsed" :collapsedWidth="0" width="200" style="background: #fff"
+    :trigger="null">
+    <a-menu mode="inline" :selectedKeys="selectedKeys" :openKeys="openKeys" @select="onSelect"
+      @openChange="onOpenChange" style="height: 100%">
+      <!-- 统一渲染所有菜单项 -->
+      <template v-for="item in menuItems" :key="item.key">
+        <!-- 渲染普通菜单项 -->
+        <a-menu-item v-if="!item.children" :key="item.key" @click.stop="handleMenuClick(item)">
+          <template #icon>
+            <component :is="item.icon" />
+          </template>
+          {{ item.label }}
         </a-menu-item>
-      </a-sub-menu>
+        <!-- 渲染带子菜单的项 -->
+        <a-sub-menu v-if="item.children" :key="item.key">
+          <template #icon>
+            <component :is="item.icon" />
+          </template>
+          <template #title>{{ item.label }}</template>
+          <a-menu-item v-for="child in item.children" :key="child.key" @click.stop="handleMenuClick(child)">
+            {{ child.label }}
+          </a-menu-item>
+        </a-sub-menu>
+      </template>
     </a-menu>
     <div class="trigger-wrapper">
-      <menu-unfold-outlined
-        v-if="collapsed"
-        class="trigger"
-        @click="() => (collapsed = !collapsed)"
-      />
-      <menu-fold-outlined
-        v-else
-        class="trigger"
-        @click="() => (collapsed = !collapsed)"
-      />
+      <menu-unfold-outlined v-if="collapsed" class="trigger" @click="() => (collapsed = !collapsed)" />
+      <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
     </div>
   </a-layout-sider>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons-vue'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  AppstoreOutlined,
+  SettingOutlined
+} from '@ant-design/icons-vue'
 
 const route = useRoute()
 const router = useRouter()
 const selectedKeys = ref(['dashboard'])
 const openKeys = ref(['test-group'])
 const collapsed = ref(false)
+
+// 监听路由 id 变化
+watch(
+  () => route.params.id,
+  (newId) => {
+    if (route.path.includes('/test')) {
+      selectedKeys.value = [newId]
+      if (newId !== 'dashboard' && newId !== 'settings') {
+        openKeys.value = ['test-group']
+      }
+      // openKeys.value = ['test-group']
+    }
+  },
+  { immediate: true }
+)
 
 // 菜单配置数据
 const menuItems = [
@@ -72,8 +70,15 @@ const menuItems = [
     path: '/test/page/dashboard'
   },
   {
+    key: 'settings',
+    label: 'Settings',
+    path: '/test/page/settings',
+    icon: SettingOutlined
+  },
+  {
     key: 'test-group',
     label: 'Test Pages',
+    icon: AppstoreOutlined,
     children: [
       {
         key: 'page1',
@@ -92,34 +97,25 @@ const menuItems = [
       }
     ]
   },
-  {
-    key: 'settings',
-    label: 'Settings',
-    path: '/test/page/settings'
-  }
+
 ]
-
-// 分离顶级菜单项和子菜单项
-const topLevelItems = computed(() => menuItems.filter(item => !item.children))
-const subMenuItems = computed(() => menuItems.filter(item => item.children))
-
 // 根据当前路由更新选中的菜单项
-const updateSelectedKeys = () => {
-  const path = route.path
-  const id = route.params.id
-  
-  if (path.includes('/test/page/')) {
-    selectedKeys.value = [id]
-    if (id !== 'dashboard' && id !== 'settings') {
-      openKeys.value = ['test-group']
-    }
-  }
-}
+// const updateSelectedKeys = () => {
+//   const path = route.path
+//   const id = route.params.id
 
-// 监听路由变化
-router.afterEach(() => {
-  updateSelectedKeys()
-})
+//   if (path.includes('/test/page/')) {
+//     selectedKeys.value = [id]
+//     if (id !== 'dashboard' && id !== 'settings') {
+//       openKeys.value = ['test-group']
+//     }
+//   }
+// }
+
+// // 监听路由变化
+// router.afterEach(() => {
+//   updateSelectedKeys()
+// })
 
 // 处理菜单点击
 const handleMenuClick = (item) => {
@@ -135,9 +131,6 @@ const onSelect = ({ selectedKeys: keys }) => {
 const onOpenChange = (keys) => {
   openKeys.value = keys
 }
-
-// 初始化选中状态
-updateSelectedKeys()
 </script>
 
 <style scoped>
@@ -173,4 +166,4 @@ updateSelectedKeys()
 .trigger:hover {
   color: #1890ff;
 }
-</style> 
+</style>
